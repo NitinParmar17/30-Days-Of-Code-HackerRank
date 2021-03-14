10b802cd17f7d734341deb38cef810c333eea69c
Nitin Parmar <77098722+NitinParmar17@users.noreply.github.com>
2021-03-14 13:45:08 +0530

    Create Day 26: Nested Logic.java

---
+++ 30 Days Of Code HackerRank/Day 26: Nested Logic.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution 
+{
+    public static void main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int d1 = sc.nextInt();
+        int m1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        
+        int d2 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int y2 = sc.nextInt();
+        
+        int fine = 0;
+        
+        if(d1<=d2 && m1==m2 && y1==y2)
+        {
+            fine =0;
+        }
+        else if(d1>d2 && m1==m2 && y1==y2)
+        {
+            fine = 15 * (d1-d2);
+        }
+        else if(m1>m2 && y1==y2)
+        {
+            fine = 500 * (m1-m2);
+        }
+        else if(y1>y2)
+        {
+            fine = 10000;
+        }
+        System.out.println(fine);
+    }
+}
